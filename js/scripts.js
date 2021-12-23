e1ecab905c6618a1bf191b76c88009f780c0b749
Kenneth Brook <kenneth.brook@hotmail.com>
2021-12-23 16:39:11 -0500

    all 4 box shadow sliders opperational

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
 const root = document.querySelector(':root');
-const slider = document.querySelector('#box-shadow-up-down');
+const slider1 = document.querySelector('#box-shadow-offset-x');
 
-slider.addEventListener('input', function(){
+slider1.addEventListener('input', function(){
 
-    root.style.setProperty("--box-shadow-offset-x", slider.value + "px");
+    root.style.setProperty("--box-shadow-offset-x", slider1.value + "px");
     
   });
+
+  const slider2 = document.querySelector('#box-shadow-offset-y');
+  
+  slider2.addEventListener('input', function(){
+  
+      root.style.setProperty("--box-shadow-offset-y", slider2.value + "px");
+      
+    });
+
+    const slider3 = document.querySelector('#box-shadow-blur');
+  
+    slider3.addEventListener('input', function(){
+    
+        root.style.setProperty("--box-shadow-blur", slider3.value + "px");
+        
+      });
+
+      const slider4 = document.querySelector('#box-shadow-spread');
+  
+      slider4.addEventListener('input', function(){
+      
+          root.style.setProperty("--box-shadow-spread", slider4.value + "px");
+          
+        });
