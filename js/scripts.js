86f5406e42eec68516695184e08ef49f31cb106e
Kenneth Brook <kenneth.brook@hotmail.com>
2021-12-25 11:29:55 -0500

    control box 1 sliders complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,36 @@
 const root = document.querySelector(':root');
+
+// Control Box 1
 const slider1 = document.querySelector('#box-shadow-offset-x');
 
 slider1.addEventListener('input', function(){
 
-    root.style.setProperty("--box-shadow-offset-x", slider1.value + "px");
+  root.style.setProperty("--box-shadow-offset-x", slider1.value + "px");
     
-  });
+});
 
-  const slider2 = document.querySelector('#box-shadow-offset-y');
+const slider2 = document.querySelector('#box-shadow-offset-y');
   
-  slider2.addEventListener('input', function(){
+slider2.addEventListener('input', function(){
   
-      root.style.setProperty("--box-shadow-offset-y", slider2.value + "px");
+  root.style.setProperty("--box-shadow-offset-y", slider2.value + "px");
       
-    });
+});
 
-    const slider3 = document.querySelector('#box-shadow-blur');
+const slider3 = document.querySelector('#box-shadow-blur');
   
-    slider3.addEventListener('input', function(){
+slider3.addEventListener('input', function(){
     
-        root.style.setProperty("--box-shadow-blur", slider3.value + "px");
+  root.style.setProperty("--box-shadow-blur", slider3.value + "px");
         
-      });
+});
 
-      const slider4 = document.querySelector('#box-shadow-spread');
+const slider4 = document.querySelector('#box-shadow-spread');
   
-      slider4.addEventListener('input', function(){
+slider4.addEventListener('input', function(){
       
-          root.style.setProperty("--box-shadow-spread", slider4.value + "px");
+  root.style.setProperty("--box-shadow-spread", slider4.value + "px");
           
-        });
+});
+
+// Control Box 2
